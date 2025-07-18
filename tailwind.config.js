1b7e5a3baaadeef5268c22c9bd8aa64cf6eebc94
BIGJONG <qkrwhdgus16@naver.com>
2025-07-18 18:14:52 +0900

    Initial commit: 프로젝트 기본 세팅 완료

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // src 폴더 내의 모든 JS/TS/JSX/TSX 파일 스캔
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
