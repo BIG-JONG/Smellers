e2dfc71b61d686c88ad9d8349b3c1716d8054833
simuneu <worksimu@gmail.com>
2025-07-21 17:53:52 +0900

    feat: dropdown component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.tsx';
+import App from './App';
 import './index.css';
-import Button from './components/Button.js';
-import Textarea from './components/Textarea';
-import Dropdown from './components/Dropdown';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Button/>
-    <br/>
-    <Textarea/>
-    <Dropdown/>
+    <App />
   </React.StrictMode>,
 );
