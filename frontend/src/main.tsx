06c2a6c3ad371203dc6497d50f65ac9c2efe72f2
BIGJONG <qkrwhdgus16@naver.com>
2025-07-21 13:26:07 +0900

    폴더추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css'; // Tailwind 지시문이 포함된 메인 CSS 파일 불러오기
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
