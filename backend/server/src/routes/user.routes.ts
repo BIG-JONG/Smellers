bafff8a382e09bafc4fddc7263575f0b88a43b95
sweetpotato <kgb4232@daum.net>
2025-07-25 13:47:34 +0900

    error 수정

--- backend/server/src/routes/user.routes.ts
+++ backend/server/src/routes/user.routes.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import express  from "express";
-import { loginController } from "../controllers/user.controller";
-import { followlistingController, followUser } from "../controllers/follow.controller";
-
-const router = express.Router();
-
-router.post('/login',loginController);
-
-// 팔로우 리스트 조회
-router.get('/:id/following', followlistingController);
-
-//팔로우 한 유저의 전체공개글 보기
-router.post('/:id/follow', followUser);
-=======
 import express from 'express';
 import { signup, login, updateUser, deleteUser } from '../controllers/user.controller';
 import { authenticateToken } from '../middlewares/auth.middleware';
