e1e5fc05c9a62364d5e5437410146b2e75bda226
sweetpotatotest <kgb4232@daum.net>
2025-07-25 14:27:05 +0900

    다시 합치기전으로

--- backend/server/src/prisma/client.ts
+++ backend/server/src/prisma/client.ts
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 import { PrismaClient } from '@prisma/client';
+=======
+import { PrismaClient } from "@prisma/client";
+>>>>>>> parent of b9d91ed (Merge branch 'origin/khg/backend' into kgb/backend)
 const prisma = new PrismaClient();
 export default prisma;
