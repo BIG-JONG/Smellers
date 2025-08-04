2da5b4c86f34223e97874b1d948800da522d83a2
simuneu <worksimu@gmail.com>
2025-08-04 19:43:54 +0900

    fix:팔로우 리스트 조회, 토큰 인증 순서 수정

--- backend/server/src/services/follow.service.ts
+++ backend/server/src/services/follow.service.ts
@@ -1,17 +1,26 @@
 import prisma from '../prisma/client';
 
-export const followListingService = async (id: number) => {
-  if (!id) throw new Error("User ID is required")
+export const followListingService = async (userId: number) => {
+  if (!userId) throw new Error("User ID is required")
 
-  const followings = await prisma.followingList.findMany({
+   const followings = await prisma.followingList.findMany({
     where: {
-      userId: id,               // 나(팔로우한 사람)
-      followStatus: 'Y',    // 활성 팔로우만
+      userId: userId,
+      followStatus: 'Y',
     },
     select: {
-      followerId: true,       // 내가 팔로우한 유저 ID만 추출
-      createdAt: true,      // 팔로우한 시각
-    },
+      followerId: true,
+      createdAt: true,
+      updatedAt: true,
+      followed: {      
+        select: {
+          nickname: true,
+          email: true,
+          profileImg: true,
+          userStatus: true,
+        }
+      }
+    }
   });
 
   return followings;
