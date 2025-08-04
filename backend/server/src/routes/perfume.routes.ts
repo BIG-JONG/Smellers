a1a4b7dc4051f2da13b62d0c612c73e49fddb541
simuneu <worksimu@gmail.com>
2025-08-04 20:55:18 +0900

    feat:향수 상세조회

--- backend/server/src/routes/perfume.routes.ts
+++ backend/server/src/routes/perfume.routes.ts
@@ -25,6 +25,7 @@ router.get('/public', getPublicPerfumes);
 router.post('/', postsValidator, authenticateToken,
    handleValidationResult, upload.array('images',1),createPerfume); // 향수 등록(파일등록 포함)
 router.get('/:perfume_id',getPostsValidator,authenticateToken,handleValidationResult, getPerfumeById); // 상세 조회
+router.get('/public/:perfume_id',getPostsValidator,handleValidationResult, getPerfumeById); // 상세 조회
 router.put('/:perfume_id', putPostsValidator,authenticateToken, 
   handleValidationResult, upload.array('images',1), updatePerfume); // 수정(파일등록 포함)
 router.delete('/:perfume_id',getPostsValidator,authenticateToken, handleValidationResult, deletePerfume); // 삭제
