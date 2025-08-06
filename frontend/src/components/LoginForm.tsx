91ce1a375ad66a535ba7deb778278f43bb9c847d
sweetpotatotest <kgb4232@daum.net>
2025-08-06 13:21:51 +0900

    error 1분뒤에 다시시도해 주세요 추가

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -6,40 +6,49 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function LoginForm() {
-  
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showAlert, setShowAlert] = useState(false);
   const [alertType, setAlertType] = useState<"info" | "success" | "error" | "warning">("info");
+  //error메세지 추가
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
-  const onSubmitForm  = async(e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    try{
-      const res = await axios.post('http://localhost:4000/users/login',{
-        email, 
+    try {
+      const res = await axios.post('http://localhost:4000/users/login', {
+        email,
         password
       })
-      if(res.status ===200){
+      if (res.status === 200) {
         const { token, user_id } = res.data;
-        
+
         sessionStorage.setItem('token', token);
         sessionStorage.setItem('user_id', JSON.stringify(user_id));
 
         setAlertType("success")
         setShowAlert(true)
 
-        setTimeout(()=>{
+        setTimeout(() => {
           navigate('/')
-        },800)
+        }, 800)
       }
-    }catch(err:any){
-      console.error("서버 응답 오류:", err.response?.data || err.message); 
-      setAlertType("error")
+    } catch (err: any) {
+      console.error("서버 응답 오류:", err.response?.data || err.message);
+
+      if (err.response?.status === 429) {
+        setErrorMessage("1분 뒤에 다시 시도해주세요.");
+      } else {
+        setErrorMessage("로그인 실패");
+      }
+
+      setAlertType("error");
       setShowAlert(true);
-      setTimeout(()=> setShowAlert(false), 2000)
-    }    
+      setTimeout(() => setShowAlert(false), 3000);
+    }
   };
 
   const handleNavigateToSignup = () => {
@@ -47,7 +56,7 @@ function LoginForm() {
   };
 
   return (
-    <form 
+    <form
       onSubmit={onSubmitForm}
       className="mt-0 flex flex-col items-center justify-center w-full max-w-screen-md bg-white p-8 rounded"> {/* max-w-3xl -> max-w-screen-md, mt-10 -> mt-4 */}
       <h1 className="text-4xl font-extrabold mb-8 text-center">로그인</h1>
@@ -75,7 +84,7 @@ function LoginForm() {
         <div className="mt-4 w-full">
           <Alert
             type={alertType}
-            message={alertType === "error" ? "로그인 실패" : "로그인 성공"}
+            message={alertType === "error" ? errorMessage : "로그인 성공"}
           />
         </div>
       )}
