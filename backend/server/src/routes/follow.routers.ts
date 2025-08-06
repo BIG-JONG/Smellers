ea388452fdec662630e3e6f0f0f6cab3d2d1a5fe
sweetpotatotest <kgb4232@daum.net>
2025-08-06 12:42:36 +0900

    언팔로우기능

--- backend/server/src/routes/follow.routers.ts
+++ backend/server/src/routes/follow.routers.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { followListingController, followUserRegist, getAllPublicPosts } from "../controllers/follow.controller";
+import { followListingController, followUserRegist, getAllPublicPosts, unfollowingController } from "../controllers/follow.controller";
 import { authenticateToken } from "../middlewares/auth.middleware";
 
 
@@ -13,5 +13,8 @@ router.get('/userRegister/:id', authenticateToken, followUserRegist);
 
 //팔로우한 유저의 전체공개 글 보기 - 토큰사용
 router.get('/allPublicPost', authenticateToken, getAllPublicPosts);
+
+//언팔로우
+router.get('/unfollow/:id', authenticateToken, unfollowingController);
 
 export default router;
