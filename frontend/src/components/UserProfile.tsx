f3502aa9405194616a95073f97a7f001a8b300d0
simuneu <worksimu@gmail.com>
2025-08-16 22:48:53 +0900

    fix:유저 프로필 변경

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -19,7 +19,8 @@ function UserProfile({
 }: UserProfileProps) {
   return (
     <div
-      className={`flex items-center gap-3 p-2 pl-8 border-b border-gray-200 hover:bg-gray-100 cursor-pointer ${className}`}
+      // className={`flex items-center gap-3 p-2 pl-8 border-b border-gray-200 hover:bg-gray-100 cursor-pointer ${className}`}
+      className={`flex items-center gap-3 p-2 pl-8  hover:bg-gray-100 cursor-pointer ${className}`}
       onClick={onClick}
     >
       <Avatar imageUrl={profileImageUrl} userId={userId} alt={alt}/>
